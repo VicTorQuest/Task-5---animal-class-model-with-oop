b7ce04581880300b0cc99afe451b582eaec8e870
Quest Coding <questcoding2001@gmail.com>
2024-02-07 10:59:06 +0100

    fixed errors

--- descriptive statistics.js
+++ descriptive statistics.js
@@ -15,7 +15,8 @@ class measureOfCentralTendency {
 
         // calculating the mean
         let mean = total / n
-        console.log('The mean for your given data is:', mean)
+        
+        return mean
     }
 
     median () {
@@ -35,7 +36,8 @@ class measureOfCentralTendency {
 
             // calculating the median by taking the average of the two middle values
             let median = (middleValue1 + middleValue2) / 2;
-            console.log('The median for the given date is:', median)
+            
+            return median
 
         // if the number of values in the dataset is odd
         } else {
@@ -43,9 +45,7 @@ class measureOfCentralTendency {
             // Find the two middle value
             let middleIndex = Math.floor(n / 2)
             let median = sortedData[middleIndex]
-
-
-            console.log('The median for the given date is:', median)
+            return median
         }
     }
 
@@ -76,16 +76,16 @@ class measureOfCentralTendency {
             }
         }
 
-        console.log('the mode of the given dataset is:', mode)
+        return(mode)
     }
 }
 
 const calculateMCT = new measureOfCentralTendency([11, 2, 17, 11, 1, 9, 12, 17, 21, 15, 14, 11, 18])
 
-exmaple
-calculateMCT.mean()
-calculateMCT.median()
-calculateMCT.mode()
+
+
+
+
 
 
 class measureOfDispersion {
@@ -102,16 +102,81 @@ class measureOfDispersion {
 
         // range is the difference between the highest and lowest value
         let range = highestValue - lowestValue
-        console.log('The range for the given data is:', range)
+        return range
     }
 
 
     variance () {
-        
+        // get the mean first
+        let getMean = new measureOfCentralTendency(this.data)
+        const mean = Math.floor(getMean.mean())
+
+        // Calculate the squared differences
+        const squaredDifferences = this.data.map(value => Math.pow(value - mean, 2));
+
+        // Find the average of squared differences (variance)
+        const variance = squaredDifferences.reduce((sum, value) => sum + value, 0) / this.data.length;
+
+        return variance;
+    }
+
+    standardDeviation () {
+        // this is the swuare root of the variance 
+        const standardDev = Math.sqrt(this.variance())
+        return standardDev
+    }
+
+    meanAbsoluteDeviation () {
+        // get the mean first
+        let getMean = new measureOfCentralTendency(this.data)
+        const mean = Math.floor(getMean.mean())
+
+        // Calculate the absolute differences
+        const absoluteDifferences = this.data.map(value => Math.abs(value - mean));
+
+        // Step 3: Find the average of absolute differences (MAD)
+        const mad = absoluteDifferences.reduce((sum, value) => sum + value, 0) / this.data.length;
+
+        return mad
+    }
+
+    interquartileRange () {
+        // sort in ascending order
+        const sortedData = this.data.sort((a, b) => a - b)
+
+        // find Q1 and Q2
+        const middleIndex = Math.floor(sortedData.length / 2);
+        const lowerHalf = sortedData.slice(0, middleIndex);
+        const upperHalf = sortedData.slice(middleIndex + (sortedData.length % 2 === 0 ? 0 : 1))
+
+        let getMedian = new measureOfCentralTendency(lowerHalf)
+        const q1 = Math.floor(getMedian.median())
+
+        getMedian = new measureOfCentralTendency(upperHalf)
+        const q3 = Math.floor(getMedian.median())
+
+        const iqr = q3 - q1
+
+        return iqr
+
     }
 }
 
 const calculateMOD = new measureOfDispersion([4, 9, 17, 1, 11, 6, 12, 13, 5, 25, 17, 15, 9, 7, 8, 12, 23, 6, 12])
 
 
-calculateMOD.range()
+// exmaples
+
+console.log('The mean for your given data is:', calculateMCT.mean())
+console.log('The median for the given date is:', calculateMCT.median())
+console.log('the mode of the given dataset is:', calculateMCT.mode())
+
+
+console.log('--------')
+
+
+console.log('The range for the given data is:', calculateMOD.range())
+console.log('The variance for the given data is:', calculateMOD.variance())
+console.log('The standard deviation for the given data is:', calculateMOD.standardDeviation())
+console.log('The mean absolute deviation for the given data is:', calculateMOD.meanAbsoluteDeviation())
+console.log('The inter quartile range for the given data is:', calculateMOD.interquartileRange())
